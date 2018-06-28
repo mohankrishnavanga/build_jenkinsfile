d996d5202188b00391905216595361fe3e273bb1
Mohan Krishna Vanga <mohankrishnav24@gmail.com>
2018-06-28 19:20:42 +0530

    Update getJson.py

--- scripts/getJson.py
+++ scripts/getJson.py
@@ -1,7 +1,8 @@
 import sys,json
 
+#workspace=sys.argv[1]
 #Read Json file and load it into a variable
-with open('..\testchannels.json','r') as f:
+with open('../testchannels.json','r') as f:
   getChannels = json.load(f)
   
 print getChannels['parentname']
