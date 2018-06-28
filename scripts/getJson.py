a4dfa9ab7cf7f609cf5f9a5379aacc6b65c53913
Mohan Krishna Vanga <mohankrishnav24@gmail.com>
2018-06-28 19:14:32 +0530

    Update getJson.py

--- scripts/getJson.py
+++ scripts/getJson.py
@@ -1,7 +1,8 @@
-import sys
+import sys,json
 
-jsonVar = sys.argv[1]
-
-echo $jsonVar
-echo $jsonVar['parentname']
-echo $jsonVar['childchannels']['childchannel1']
+#Read Json file and load it into a variable
+with open('../testchannels.json','r') as f:
+  getChannels = json.load(f)
+  
+print getChannels['parentname']
+print getChannels['childchannels']['childchannel1']
